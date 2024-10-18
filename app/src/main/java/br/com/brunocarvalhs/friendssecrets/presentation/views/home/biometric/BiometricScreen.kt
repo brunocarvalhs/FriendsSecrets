329b836c5f313f5fe2155094482bff038841d1ca
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2024-10-18 16:22:28 -0300

    feat: biometric to face and password

--- app/src/main/java/br/com/brunocarvalhs/friendssecrets/presentation/views/home/biometric/BiometricScreen.kt
+++ app/src/main/java/br/com/brunocarvalhs/friendssecrets/presentation/views/home/biometric/BiometricScreen.kt
@@ -3,6 +3,8 @@ package br.com.brunocarvalhs.friendssecrets.presentation.views.home.biometric
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_WEAK
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -23,11 +25,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavHostController
+import br.com.brunocarvalhs.friendssecrets.R
 import br.com.brunocarvalhs.friendssecrets.presentation.ui.theme.FriendsSecretsTheme
 import br.com.brunocarvalhs.friendssecrets.presentation.views.home.HomeNavigation
 import timber.log.Timber
@@ -66,10 +70,8 @@ fun BiometricScreen(
         })
 
     val promptInfo = BiometricPrompt.PromptInfo.Builder()
-        .setAllowedAuthenticators(BIOMETRIC_STRONG)
-        .setTitle("Biometric Authentication")
-        .setSubtitle("Log in using your biometric credential")
-        .setNegativeButtonText("Use account password")
+        .setAllowedAuthenticators(BIOMETRIC_STRONG or BIOMETRIC_WEAK or DEVICE_CREDENTIAL)
+        .setTitle(stringResource(R.string.biometric_screen_title_authentication))
         .build()
 
     LaunchedEffect(Unit) {
@@ -99,13 +101,13 @@ private fun BiometricContent(
             ) {
                 Icon(Icons.Filled.Lock, contentDescription = null)
                 Spacer(modifier = Modifier.height(16.dp))
-                Text(text = "Biometric Authentication")
+                Text(text = stringResource(R.string.biometric_screen_biometric_authentication))
             }
             TextButton(
                 onClick = onClick,
                 modifier = Modifier.padding(16.dp)
             ) {
-                Text(text = "Desbloquear")
+                Text(text = stringResource(R.string.biometric_screen_unlock))
             }
         }
     }
