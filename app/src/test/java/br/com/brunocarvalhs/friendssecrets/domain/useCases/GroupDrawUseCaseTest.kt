1313e03450a64cf02b8be3b4ed20ec40352fcfc2
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2024-10-18 23:18:15 -0300

    feat: adapter tests units

--- app/src/test/java/br/com/brunocarvalhs/friendssecrets/domain/useCases/GroupDrawUseCaseTest.kt
+++ app/src/test/java/br/com/brunocarvalhs/friendssecrets/domain/useCases/GroupDrawUseCaseTest.kt
@@ -1,11 +1,14 @@
 package br.com.brunocarvalhs.friendssecrets.domain.useCases
 
 import br.com.brunocarvalhs.friendssecrets.CustomApplication
+import br.com.brunocarvalhs.friendssecrets.commons.performance.PerformanceManager
 import br.com.brunocarvalhs.friendssecrets.domain.entities.GroupEntities
 import br.com.brunocarvalhs.friendssecrets.domain.repository.GroupRepository
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.mockkObject
+import io.mockk.runs
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
@@ -14,20 +17,25 @@ class GroupDrawUseCaseTest {
 
     private lateinit var mockContext: CustomApplication
     private lateinit var repository: GroupRepository
+    private lateinit var performance: PerformanceManager
     private lateinit var useCase: GroupDrawUseCase
 
     @Before
     fun setup() {
         mockContext = mockk(relaxed = true)
         repository = mockk(relaxed = true)
+        performance = mockk(relaxed = true)
 
         mockkObject(CustomApplication)
         every { CustomApplication.getInstance() } returns mockContext
         every { mockContext.getString(any()) } returns "Test String"
+        every { performance.start(any()) } just runs
+        every { performance.stop(any()) } just runs
 
         useCase = GroupDrawUseCase(
             context = mockContext,
-            groupRepository = repository
+            groupRepository = repository,
+            performance = performance
         )
     }
 
