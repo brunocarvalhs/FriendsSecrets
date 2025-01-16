d55e2463ff1769492b8afa3a912af58c50b2a94d
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2025-01-15 21:16:35 -0300

    Fix: versão 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 24
         targetSdk = 35
 
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
