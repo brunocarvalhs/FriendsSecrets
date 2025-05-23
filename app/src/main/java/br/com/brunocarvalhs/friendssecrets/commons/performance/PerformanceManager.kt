fb53de90a0c67b107a68099d5dfcd4db64a07380
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2025-05-23 17:56:32 -0300

    feat: refatorando método de PerformanceManager

--- app/src/main/java/br/com/brunocarvalhs/friendssecrets/commons/performance/PerformanceManager.kt
+++ app/src/main/java/br/com/brunocarvalhs/friendssecrets/commons/performance/PerformanceManager.kt
@@ -3,15 +3,27 @@ package br.com.brunocarvalhs.friendssecrets.commons.performance
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.perf.FirebasePerformance
 import com.google.firebase.perf.ktx.performance
+import com.google.firebase.perf.metrics.AddTrace
+import com.google.firebase.perf.metrics.Trace
 
 class PerformanceManager(
     private val firebasePerformance: FirebasePerformance = Firebase.performance
 ) {
+
+    private val traces = mutableMapOf<String, Trace>()
+
+    @AddTrace(name = "PerformanceManager.start")
     fun start(name: String) {
-        firebasePerformance.newTrace(name).start()
+        val trace = firebasePerformance.newTrace(name)
+        trace.start()
+        traces[name] = trace
     }
 
+    @AddTrace(name = "PerformanceManager.stop")
     fun stop(name: String) {
-        firebasePerformance.newTrace(name).stop()
+        traces[name]?.run {
+            this.stop()
+            traces.remove(name)
+        }
     }
 }
