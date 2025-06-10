8fafd22e842f449bfd5423a3cb730018af43230c
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2025-06-10 18:47:28 -0300

    feat: novos campos de usuário

--- core/data/src/main/java/br/com/brunocarvalhs/friendssecrets/data/model/UserModel.kt
+++ core/data/src/main/java/br/com/brunocarvalhs/friendssecrets/data/model/UserModel.kt
@@ -12,6 +12,9 @@ internal data class UserModel(
     @SerializedName(UserEntities.PHONE_NUMBER) override val phoneNumber: String = "",
     @SerializedName(UserEntities.IS_PHONE_NUMBER_VERIFIED) override val isPhoneNumberVerified: Boolean = false,
     @SerializedName(UserEntities.LIKES) override val likes: List<String> = emptyList(),
+    @SerializedName(UserEntities.IS_ANONYMOUS) override val isAnonymous: Boolean = false,
+    @SerializedName(UserEntities.LAST_LOGIN) override val lastLogin: Long = System.currentTimeMillis(),
+    @SerializedName(UserEntities.IS_ACTIVE) override val isActive: Boolean = true
 ) : UserEntities {
 
     override fun toMap(): Map<String, Any> {
@@ -21,7 +24,10 @@ internal data class UserModel(
             UserEntities.PHOTO_URL to photoUrl.orEmpty(),
             UserEntities.PHONE_NUMBER to phoneNumber,
             UserEntities.IS_PHONE_NUMBER_VERIFIED to isPhoneNumberVerified,
-            UserEntities.LIKES to likes
+            UserEntities.LIKES to likes,
+            UserEntities.IS_ANONYMOUS to isAnonymous,
+            UserEntities.LAST_LOGIN to lastLogin,
+            UserEntities.IS_ACTIVE to isActive
         )
     }
 
@@ -31,14 +37,20 @@ internal data class UserModel(
         photoUrl: String?,
         phoneNumber: String,
         isPhoneNumberVerified: Boolean,
-        likes: List<String>
+        likes: List<String>,
+        isAnonymous: Boolean,
+        lastLogin: Long,
+        isActive: Boolean
     ): UserEntities = copy(
         id = id,
         name = name,
         photoUrl = photoUrl,
         phoneNumber = phoneNumber,
         isPhoneNumberVerified = isPhoneNumberVerified,
-        likes = likes
+        likes = likes,
+        isAnonymous = isAnonymous,
+        lastLogin = lastLogin,
+        isActive = isActive
     )
 
     companion object {
@@ -57,12 +69,18 @@ fun UserEntities.Companion.create(
     photoUrl: String? = null,
     phoneNumber: String = "",
     isPhoneNumberVerified: Boolean = false,
-    likes: List<String> = emptyList()
+    likes: List<String> = emptyList(),
+    isAnonymous: Boolean = false,
+    lastLogin: Long = System.currentTimeMillis(),
+    isActive: Boolean = true
 ): UserEntities = UserModel(
     id = id,
     name = name,
     photoUrl = photoUrl,
     phoneNumber = phoneNumber,
     isPhoneNumberVerified = isPhoneNumberVerified,
-    likes = likes
+    likes = likes,
+    isAnonymous = isAnonymous,
+    lastLogin = lastLogin,
+    isActive = isActive
 )
--- core/domain/src/main/java/br/com/brunocarvalhs/friendssecrets/domain/entities/UserEntities.kt
+++ core/domain/src/main/java/br/com/brunocarvalhs/friendssecrets/domain/entities/UserEntities.kt
@@ -7,6 +7,9 @@ interface UserEntities {
     val phoneNumber: String
     val isPhoneNumberVerified: Boolean
     val likes: List<String>
+    val isAnonymous: Boolean
+    val lastLogin: Long
+    val isActive: Boolean
 
     fun toMap(): Map<String, Any>
 
@@ -16,7 +19,10 @@ interface UserEntities {
         photoUrl: String? = this.photoUrl,
         phoneNumber: String = this.phoneNumber,
         isPhoneNumberVerified: Boolean = this.isPhoneNumberVerified,
-        likes: List<String> = this.likes
+        likes: List<String> = this.likes,
+        isAnonymous: Boolean = this.isAnonymous,
+        lastLogin: Long = this.lastLogin,
+        isActive: Boolean = this.isActive
     ): UserEntities
 
     companion object {
@@ -28,5 +34,8 @@ interface UserEntities {
         const val PHONE_NUMBER = "phoneNumber"
         const val IS_PHONE_NUMBER_VERIFIED = "isPhoneNumberVerified"
         const val LIKES = "likes"
+        const val IS_ANONYMOUS = "isAnonymous"
+        const val LAST_LOGIN = "lastLogin"
+        const val IS_ACTIVE = "isActive"
     }
 }
