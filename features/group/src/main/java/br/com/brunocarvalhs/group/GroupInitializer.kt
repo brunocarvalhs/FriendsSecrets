627bd2fe8ff9e60dde47b1502fe7dd5f456a1a43
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2025-07-01 21:07:20 -0300

    feat: incluindo trace de performance de cada método no modulo de group

--- features/group/src/main/java/br/com/brunocarvalhs/group/GroupInitializer.kt
+++ features/group/src/main/java/br/com/brunocarvalhs/group/GroupInitializer.kt
@@ -4,10 +4,12 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import br.com.brunocarvalhs.friendssecrets.common.remote.toggle.ToggleManager
 import br.com.brunocarvalhs.group.commons.navigation.groupGraph
+import com.google.firebase.perf.metrics.AddTrace
 import kotlin.properties.Delegates
 
 class GroupInitializer(private val builder: Builder) {
 
+    @AddTrace(name = "GroupInitializer.build", enabled = true)
     private fun build(navGraphBuilder: NavGraphBuilder) {
         return navGraphBuilder.groupGraph(
             navController = builder.navController
@@ -18,12 +20,15 @@ class GroupInitializer(private val builder: Builder) {
         internal var toggleManager: ToggleManager by Delegates.notNull()
         internal var navController: NavHostController by Delegates.notNull()
 
+        @AddTrace(name = "GroupInitializer.Builder.navController", enabled = true)
         fun navController(navController: NavHostController) =
             apply { this.navController = navController }
 
+        @AddTrace(name = "GroupInitializer.Builder.toggleManager", enabled = true)
         fun toggleManager(toggleManager: ToggleManager) =
             apply { this.toggleManager = toggleManager }
 
+        @AddTrace(name = "GroupInitializer.Builder.build", enabled = true)
         fun build(navGraphBuilder: NavGraphBuilder): GroupInitializer =
             GroupInitializer(this).also { it.build(navGraphBuilder) }
     }
