ee96096fa3a0de7cc7dd45ae11d3125f49f96925
Bruno Carvalho <bruno.silva.carvalho@outlook.com.br>
2025-07-01 21:14:42 -0300

    feat: versão mirror de atualização

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         //noinspection OldTargetApi
         targetSdk = 35
 
-        versionCode = 9
-        versionName = "2.0.1"
+        versionCode = 11
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
