061996a324cf10b5cb16f2f2d0da3d08fd680d5c
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2026-04-14 16:54:27 -0300

    test: unit tests para rotas de navegação de settings
    - Remove ExampleUnitTest
    - Adiciona testes de serialização para FAQScreenRoute
    - Adiciona testes de serialização para AppearanceScreenRoute
    - Adiciona testes de serialização para SettingsScreenRoute
    - Adiciona testes de serialização para SettingsGraphRoute
    - Adiciona testes de serialização para ReportIssueScreenRoute

---
+++ features/settings/src/test/java/br/com/brunocarvalhs/settings/commons/navigation/FAQScreenRouteTest.kt
@@ -0,0 +1,12 @@
+package br.com.brunocarvalhs.settings.commons.navigation
+
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
+class FAQScreenRouteTest {
+
+    @Test
+    fun `route should be serializable`() {
+        assertNotNull(FAQScreenRoute)
+    }
+}
---
+++ features/settings/src/test/java/br/com/brunocarvalhs/settings/commons/navigation/SettingsGraphRouteTest.kt
@@ -0,0 +1,12 @@
+package br.com.brunocarvalhs.settings.commons.navigation
+
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+
+class SettingsGraphRouteTest {
+
+    @Test
+    fun `route should be serializable`() {
+        assertNotNull(SettingsGraphRoute)
+    }
+}
