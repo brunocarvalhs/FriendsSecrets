495ba64e811156a517386550d4b09ca2a0a2f5a2
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2026-04-26 17:47:45 -0300

    refactor: analytics do fluxo de navegação

--- app/src/main/java/br/com/brunocarvalhs/friendssecrets/MainActivity.kt
+++ app/src/main/java/br/com/brunocarvalhs/friendssecrets/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.brunocarvalhs.friendssecrets
 
+import AnalyticsParam
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
@@ -15,9 +17,11 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import br.com.brunocarvalhs.biometric.BiometricService
+import br.com.brunocarvalhs.core.analytics.AnalyticsService
+import br.com.brunocarvalhs.core.analytics.commons.AnalyticsEvent
+import br.com.brunocarvalhs.core.navigation.FeatureInitializer
 import br.com.brunocarvalhs.core.remote.domain.ThemeRemote
 import br.com.brunocarvalhs.core.remote.domain.ThemeService
-import br.com.brunocarvalhs.core.navigation.FeatureInitializer
 import br.com.brunocarvalhs.core.ui.theme.FriendsSecretsTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -32,6 +36,8 @@ class MainActivity : FragmentActivity() {
     lateinit var themeRemoteProvider: ThemeRemote
     @Inject
     lateinit var biometricService: BiometricService
+    @Inject
+    lateinit var analyticsService: AnalyticsService
     @Inject
     lateinit var featureInitializers: Set<@JvmSuppressWildcards FeatureInitializer>
 
@@ -60,6 +66,19 @@ class MainActivity : FragmentActivity() {
                     color = MaterialTheme.colorScheme.background,
                 ) {
                     val navController = rememberNavController()
+
+                    LaunchedEffect(navController) {
+                        navController.currentBackStackEntryFlow.collect { backStackEntry ->
+                            val route = backStackEntry.destination.route
+                            analyticsService.logEvent(
+                                name = AnalyticsEvent.VIEW,
+                                params = mapOf(
+                                    AnalyticsParam.SCREEN to route
+                                )
+                            )
+                        }
+                    }
+
                     navController.mainApp(
                         isBiometric = biometricService.isBiometricPromptEnabled.collectAsState().value,
                         initializers = featureInitializers
